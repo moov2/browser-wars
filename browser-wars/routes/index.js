a42f4712f4695e0534f5d692c3c50a3e17db075f
Lawrence Dine <lawrence@moov2.com>
2013-07-06 13:31:38 +0100

    Index page now uses references to get the title

--- browser-wars/routes/index.js
+++ browser-wars/routes/index.js
@@ -2,7 +2,8 @@
 /*
  * GET home page.
  */
+var title = require('../references').title;
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: title });
 };
